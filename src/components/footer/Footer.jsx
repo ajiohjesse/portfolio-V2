660181901da85886f6087902088e73c64d765e2f
Jesse Ajioh <ajiohjesse@gmail.com>
2023-01-03 02:29:28 +0000

    Add contact and footer section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,20 @@
+import Container from '@components/container/Container'
+import { FiHeart } from 'react-icons/fi'
 import styles from './footer.module.css'
 
 const Footer = () => {
-  return <footer>footer</footer>
+  return (
+    <footer className={styles.footer}>
+      <Container>
+        <div className={styles.wrapper}>
+          <p>
+            &copy;{new Date().getFullYear()}. Designed with <FiHeart /> by Jesse
+            Ajioh
+          </p>
+        </div>
+      </Container>
+    </footer>
+  )
 }
 
 export default Footer
