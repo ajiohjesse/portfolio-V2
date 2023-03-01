984ce0d1188067cbf4394f62864ebb94122a7d0c
Jesse Ajioh <ajiohjesse@gmail.com>
2023-03-01 11:07:37 +0100

    Add google meta tag

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,22 +1,29 @@
-import Contact from "@components/contact/Contact";
-import Hero from "@components/hero/Hero";
-import Interests from "@components/interests/Interests";
-import Intro from "@components/intro/Intro";
-import LatestPosts from "@components/latestPosts/LatestPosts";
-import Projects from "@components/projects/Projects";
-import SEO from "@components/SEO";
-import Tools from "@components/tools/Tools";
-import { getAllFilesFrontMatter } from "@helpers/mdx";
-import useScrollAnimate from "@hooks/useScrollAnimate";
+import Contact from '@components/contact/Contact';
+import Hero from '@components/hero/Hero';
+import Interests from '@components/interests/Interests';
+import Intro from '@components/intro/Intro';
+import LatestPosts from '@components/latestPosts/LatestPosts';
+import Projects from '@components/projects/Projects';
+import SEO from '@components/SEO';
+import Tools from '@components/tools/Tools';
+import { getAllFilesFrontMatter } from '@helpers/mdx';
+import useScrollAnimate from '@hooks/useScrollAnimate';
+import Head from 'next/head';
 
 export default function Home({ posts }) {
-  useScrollAnimate("scale", "scale-in");
-  useScrollAnimate("slide-left", "slide-left-in");
-  useScrollAnimate("slide-right", "slide-right-in");
+  useScrollAnimate('scale', 'scale-in');
+  useScrollAnimate('slide-left', 'slide-left-in');
+  useScrollAnimate('slide-right', 'slide-right-in');
 
   return (
     <>
       <SEO />
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="bAU-_HqkWf9DGH6BlWDVxu4TOq4cbj_dIFuX0VxHXFw"
+        />
+      </Head>
       <main>
         <Hero />
         <Intro />
@@ -31,7 +38,7 @@ export default function Home({ posts }) {
 }
 
 export const getStaticProps = async () => {
-  const posts = await getAllFilesFrontMatter("blog");
+  const posts = await getAllFilesFrontMatter('blog');
 
   const firstFourPosts = posts.slice(0, 4);
 
